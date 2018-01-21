f8e112a88cb42c1a3ed443e2ecef2d9e9cfb0ce2
Chris S <christopher.supinger@marquette.edu>
2018-01-21 12:58:53 -0600

    City of Mke Municipal Court Scrape
    Recursive scraping program for recording adjudication data for cases processed through Mke Municipal Court.

---
+++ mke_muni_court/mkemunicourtdb.py
@@ -0,0 +1,68 @@
+from sqlalchemy import create_engine, Float, ForeignKey, Column, Date, DateTime, String
+from sqlalchemy.ext.declarative import declarative_base
+
+db = create_engine('sqlite:///mkemunicourt.db', echo = False)
+Base = declarative_base()
+
+class MkeMuniCourt(Base):
+
+    __tablename__ = 'mke_muni_court'
+
+    # Defendent Info
+    d_case_no = Column(String, primary_key = True, nullable = False)
+    d_name = Column(String)
+    #d_last_known_addr = Column(String)
+    d_mo_yr_birth = Column(Date)
+    d_sex = Column(String)
+    d_race = Column(String)
+
+    # Case Info
+    c_case_type = Column(String)
+    c_violation = Column(String)
+    c_violation_date = Column(DateTime)
+    c_location = Column(String)
+    c_plea = Column(String)
+    c_plea_ent_by = Column(String)
+    c_status = Column(String)
+    c_citation_no = Column(String)
+    c_deposit_amt = Column(Float)
+    c_in_collection = Column(String)
+    c_installment_plan = Column(String)
+
+    # Judgement
+    j_finding = Column(String)
+    j_penalty = Column(Float)
+    j_balance_due = Column(Float)
+    j_date = Column(Date)
+    j_due_on_date = Column(Date)
+    j_branch = Column(String)
+
+    def __init__(self, d_case_no, d_name, d_mo_yr_birth, d_sex, d_race, c_case_type,\
+    c_violation, c_violation_date, c_location, c_plea, c_plea_ent_by, c_status,\
+    c_citation_no, c_deposit_amt, c_in_collection, c_installment_plan, j_finding,\
+    j_penalty, j_balance_due, j_date, j_due_on_date, j_branch):
+        self.d_case_no = d_case_no
+        self.d_name = d_name
+        #self.d_last_known_addr = d_last_known_addr
+        self.d_mo_yr_birth = d_mo_yr_birth
+        self.d_sex = d_sex
+        self.d_race = d_race
+        self.c_case_type = c_case_type
+        self.c_violation = c_violation
+        self.c_violation_date = c_violation_date
+        self.c_location = c_location
+        self.c_plea = c_plea
+        self.c_plea_ent_by = c_plea_ent_by
+        self.c_status = c_status
+        self.c_citation_no = c_citation_no
+        self.c_deposit_amt = c_deposit_amt
+        self.c_in_collection = c_in_collection
+        self.c_installment_plan = c_installment_plan
+        self.j_finding = j_finding
+        self.j_penalty = j_penalty
+        self.j_balance_due = j_balance_due
+        self.j_date = j_date
+        self.j_due_on_date = j_due_on_date
+        self.j_branch = j_branch
+
+Base.metadata.create_all(db)
---
+++ mke_muni_court/s_mkemunicourt.py
@@ -0,0 +1,97 @@
+from fake_useragent import UserAgent
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+from datetime import datetime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from mkemunicourtdb import *
+
+import random
+import time
+
+db = create_engine('sqlite:///mkemunicourt.db', echo = False)
+
+Session = sessionmaker(bind = db)
+session = Session()
+
+ch_opt = webdriver.ChromeOptions()
+ch_opt.add_argument('headless')
+ch_opt.add_argument('user-agent = UserAgent().random')
+
+ch_dr = webdriver.Chrome(chrome_options = ch_opt)
+
+def s_mkemunicourt(c_no):
+
+    ch_dr.get("https://query.municourt.milwaukee.gov/")
+    case_no_in = ch_dr.find_element_by_id("QuerySection_DisclaimerSection_DisclaimerAgree")
+    case_no_in.send_keys(Keys.RETURN)
+    case_no = ch_dr.find_element_by_id("QuerySection_MainSearchSection_txtCaseNumber")
+    case_no.send_keys(c_no)
+    case_no.send_keys(Keys.RETURN)
+
+    try:
+
+        #case_no = ch_dr.find_element_by_id("QuerySection_CaseDetailSection_lblCaseNumber").text
+        d_name = ch_dr.find_element_by_id("QuerySection_CaseDetailSection_lblDefendantName").text
+        #last_known_addr = ch_dr.find_element_by_id("QuerySection_CaseDetailSection_lblAddress").text
+        d_mo_yr_birth = ch_dr.find_element_by_id("QuerySection_CaseDetailSection_lblBirthDate").text
+        d_sex = ch_dr.find_element_by_id("QuerySection_CaseDetailSection_lblSex").text
+        d_race = ch_dr.find_element_by_id("QuerySection_CaseDetailSection_lblRace").text
+
+        c_case_type = ch_dr.find_element_by_id("QuerySection_CaseDetailSection_lblCaseType").text
+        c_violation = ch_dr.find_element_by_id("QuerySection_CaseDetailSection_lblViolation").text
+        c_violation_date = ch_dr.find_element_by_id("QuerySection_CaseDetailSection_lblViolationDateTime").text
+        c_location = ch_dr.find_element_by_id("QuerySection_CaseDetailSection_lblLocation").text
+        c_plea = ch_dr.find_element_by_id("QuerySection_CaseDetailSection_lblPlea").text
+        c_plea_ent_by = ch_dr.find_element_by_id("QuerySection_CaseDetailSection_lblPleaEnteredBy").text
+        c_status = ch_dr.find_element_by_id("QuerySection_CaseDetailSection_lblStatus").text
+        c_citation_no = ch_dr.find_element_by_id("QuerySection_CaseDetailSection_lblCitationNumber").text
+        c_deposit_amt = ch_dr.find_element_by_id("QuerySection_CaseDetailSection_lblDepositAmount").text
+        c_in_collection = ch_dr.find_element_by_id("QuerySection_CaseDetailSection_lblCollection").text
+        c_installment_plan = ch_dr.find_element_by_id("QuerySection_CaseDetailSection_lblInstallment").text
+
+        j_finding = ch_dr.find_element_by_id("QuerySection_CaseDetailSection_lblFinding").text
+        j_penalty = ch_dr.find_element_by_id("QuerySection_CaseDetailSection_lblTotalJudgmentAmt").text
+        j_balance_due = ch_dr.find_element_by_id("QuerySection_CaseDetailSection_lblBalanceDue").text
+        j_date = ch_dr.find_element_by_id("QuerySection_CaseDetailSection_lblJudgmentDate").text
+        j_due_on_date = ch_dr.find_element_by_id("QuerySection_CaseDetailSection_lblDueOn").text
+        j_branch = ch_dr.find_element_by_id("QuerySection_CaseDetailSection_lblBranch").text
+
+        f_d_mo_yr_birth = '%m/%Y'
+        d_1 = datetime.strptime(d_mo_yr_birth, f_d_mo_yr_birth)
+        f_c_violation_date = '%m/%d/%Y %I:%M %p'
+        d_2 = datetime.strptime(c_violation_date, f_c_violation_date)
+        f_j_date = '%m/%d/%Y'
+        d_3 = datetime.strptime(j_date, f_j_date)
+        f_j_due_on_date = '%m/%d/%Y'
+        d_4 = datetime.strptime(j_due_on_date, f_j_due_on_date)
+        f_c_deposit_amt = float(c_deposit_amt[1:])
+        f_j_penalty = float(j_penalty[1:])
+        f_j_balance_due = float(j_balance_due[1:])
+
+
+        record = MkeMuniCourt(c_no, d_name, d_1, d_sex, d_race, c_case_type, c_violation, d_2, c_location,\
+        c_plea, c_plea_ent_by, c_status, c_citation_no, f_c_deposit_amt, c_in_collection, c_installment_plan,\
+        j_finding, f_j_penalty, f_j_balance_due, d_3, d_4, j_branch)
+        session.add(record)
+        session.commit()
+
+        print(c_no, ' passed.')
+        time.sleep(3)
+
+    except:
+
+        record = MkeMuniCourt(c_no, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None)
+        session.add(record)
+        session.commit()
+
+        print(c_no, ' passed, but has conflicts.')
+        time.sleep(3)
+        pass
+
+    if c_no < n:
+
+        s_mkemunicourt(c_no + 1)
+
+s_mkemunicourt(n)
